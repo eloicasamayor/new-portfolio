a4442b789cb1098343be4c7ab1a0b736a31cb55f
eloicasamayor <37459666+eloicasamayor@users.noreply.github.com>
2024-07-11 20:59:11 +0200

    he desistit de lidioma automatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,8 @@ export default defineConfig({
   integrations: [tailwind()],
   site: "https://eloicasamayor.github.io",
   base: "new-portfolio",
-  defaultLocale: "ca",
-  locales: ["es", "en", "ca", "ca-ES"],
-  routing: {
-    prefixDefaultLocale: false,
+  i18n: {
+    defaultLocale: "ca",
+    locales: ["es", "en", "ca"],
   },
 });
